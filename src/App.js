ed55dc17cab57b0c85f86905062cf6d2633a7370
LAPTOP-UI8QJVQ5\HOME <phuongcf1998@gmail.com>
2022-05-04 16:20:49 +0700

    Update app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         flexDirection: 'column',
         backgroundColor: 'white',
         padding: 20,
-        boxShadow: '0 0 10px 4px #bfbfbf',
+      
         borderRadius: 5,
         height: '90vh',
       
